c5eb094aa481a874b6f5825185881e370cce5e18
Mauro Bellati <mauro.bellati@gmail.com>
2019-12-05 18:24:13 +0900

    Day 03 - Part 2

--- src/main/java/adventofcode/y2019/Day03.java
+++ src/main/java/adventofcode/y2019/Day03.java
@@ -1,16 +1,16 @@
 package adventofcode.y2019;
 
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Sets.intersection;
 import static com.google.common.collect.Sets.newHashSet;
 import static java.lang.Math.abs;
 import static java.lang.System.out;
 import static java.util.Arrays.asList;
-import static java.util.Comparator.comparing;
+import static java.util.Collections.unmodifiableList;
 import static java.util.stream.Collectors.toList;
-import static java.util.stream.Collectors.toSet;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import com.google.common.collect.Sets;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Value;
@@ -20,6 +20,7 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -70,6 +71,38 @@ Here are a few more examples:
 
 What is the Manhattan distance from the central port to the closest intersection?
 
+--- Part Two ---
+
+It turns out that this circuit is very timing-sensitive; you actually need to minimize the signal delay.
+
+To do this, calculate the number of steps each wire takes to reach each intersection; choose the intersection where the sum of both wires' steps is lowest. If a wire visits a position on the grid multiple times, use the steps value from the first time it visits that position when calculating the total value of a specific intersection.
+
+The number of steps a wire takes is the total number of grid squares the wire has entered to get to that location, including the intersection being considered. Again consider the example from above:
+
+...........
+.+-----+...
+.|.....|...
+.|..+--X-+.
+.|..|..|.|.
+.|.-X--+.|.
+.|..|....|.
+.|.......|.
+.o-------+.
+...........
+
+In the above example, the intersection closest to the central port is reached after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps by the second wire for a total of 20+20 = 40 steps.
+
+However, the top-right intersection is better: the first wire takes only 8+5+2 = 15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30 steps.
+
+Here are the best steps for the extra examples from above:
+
+    R75,D30,R83,U83,L12,D49,R71,U7,L72
+    U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+    R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+    U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+
+What is the fewest combined steps the wires must take to reach an intersection?
+
  */
 @SuppressWarnings("FieldNamingConvention")
 class Day03 extends Base {
@@ -94,6 +127,7 @@ class Day03 extends Base {
     static Stream<Arguments> examples() {
       return Stream.of(
         arguments(asList("R3, U3", "U6, R3, D3"), 6),
+        arguments(asList("R8, U5, L5, D3", "U7, R6, D4, L4"), 6),
         arguments(asList("R75, D30, R83, U83, L12, D49, R71, U7, L72", "U62, R66, U55, R34, D71, R55, D58, R83"), 159),
         arguments(asList("R98, U47, R26, D63, R33, U87, L62, D20, R33, U53, R51", "U98, R91, D20, R16, D67, R40, U7, R15, U6, R7"), 135)
       );
@@ -103,6 +137,23 @@ class Day03 extends Base {
     @MethodSource
     void examples(final List<String> input, Integer expected) {
       assertThat(new Day03(input).part1()).isEqualTo(expected);
+    }
+  }
+
+  static class Part2Test {
+    static Stream<Arguments> examples() {
+      return Stream.of(
+        arguments(asList("R3, U3", "U6, R3, D3"), 18),
+        arguments(asList("R8, U5, L5, D3", "U7, R6, D4, L4"), 30),
+        arguments(asList("R75, D30, R83, U83, L12, D49, R71, U7, L72", "U62, R66, U55, R34, D71, R55, D58, R83"), 610),
+        arguments(asList("R98, U47, R26, D63, R33, U87, L62, D20, R33, U53, R51", "U98, R91, D20, R16, D67, R40, U7, R15, U6, R7"), 410)
+      );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void examples(final List<String> input, Integer expected) {
+      assertThat(new Day03(input).part2()).isEqualTo(expected);
     }
   }
 
@@ -155,46 +206,79 @@ class Day03 extends Base {
     }
   }
 
+  @Value
+  private static class Wire {
+    final List<Point> points;
+
+    Wire(final List<Point> points) {
+      this.points = unmodifiableList(points);
+    }
+
+    static Wire build(final Point origin, final Collection<Instruction> instructions) {
+      List<Point> result = newArrayList();
+      var p = origin;
+      for (var instruction : instructions) {
+        var wireSegment = p.pathTowards(instruction.getDirection(), instruction.getDistance());
+        result.addAll(wireSegment.collect(toList()));
+        p = p.move(instruction);
+      }
+      return new Wire(result);
+    }
+
+    Integer distanceOf(final Point p) {
+      return Optional.of(points.indexOf(p) + 1)
+                     .filter(it -> it > 0)
+                     .orElseThrow(() -> new IllegalStateException(p + " not found"));
+    }
+
+    Set<Point> intersect(final Wire other) {
+      return intersection(newHashSet(getPoints()), newHashSet(other.getPoints()));
+    }
+  }
+
+  @Value
+  @AllArgsConstructor
+  private static class WirePoint {
+    private Point point;
+    private int step;
+  }
+
+  private final Wire wire0;
+  private final Wire wire1;
+
   Day03(final List<String> inputLines) {
     super(inputLines);
+    wire0 = wire(0);
+    wire1 = wire(1);
   }
 
   public static void main(String args[]) {
-    out.println(new Day03(inputForDay(3)).part1());
-    //    out.println(new Day03(inputForDay(2)).part2());
-  }
-
-  private static Point findClosestTo(final Collection<Point> points, final Point target) {
-    return points.stream()
-                 .min(comparing(it -> Point.manhattanDistance(it, target)))
-                 .orElseThrow();
+    out.println(new Day03(inputForDay(3)).part1()); // 1084
+    out.println(new Day03(inputForDay(3)).part2()); // 9240
   }
 
   Integer part1() {
-    var wires = inputStream().map(it -> splitAndMap(it, ",", Instruction::parse)).collect(toList());
-    var closestIntersection = findClosestTo(intersect(wires), Point.ORIGIN);
-    return Point.manhattanDistance(Point.ORIGIN, closestIntersection);
+    return wire0.intersect(wire1)
+                .stream()
+                .mapToInt(it -> Point.manhattanDistance(it, Point.ORIGIN))
+                .min()
+                .orElseThrow(() -> new IllegalStateException("No intersection"));
   }
 
   Integer part2() {
-    throw new IllegalStateException("Unable to find a valid solution");
+    return wire0.intersect(wire1)
+                .stream()
+                .mapToInt(it -> wire0.distanceOf(it) + wire1.distanceOf(it))
+                .min()
+                .orElseThrow(() -> new IllegalStateException("No intersection"));
   }
 
-  private Set<Point> intersect(final List<List<Instruction>> wires) {
-    return wires.stream()
-                .map(this::wirePath)
-                .reduce(Sets::intersection).orElseThrow();
+  private Collection<Instruction> instructions(final int index) {
+    return splitAndMap(inputList().get(index), ",", Instruction::parse);
   }
 
-  private Set<Point> wirePath(final List<Instruction> wire) {
-    Set<Point> result = newHashSet();
-    var p = Point.ORIGIN;
-    for (var instruction : wire) {
-      var path = p.pathTowards(instruction.getDirection(), instruction.getDistance());
-      result.addAll(path.collect(toSet()));
-      p = p.move(instruction);
-    }
-    return result;
+  private Wire wire(final int index) {
+    return Wire.build(Point.ORIGIN, instructions(index));
   }
 
 }
